108f79234102e1c2108670bed98a6ae93832e022
Robert Young <robeyoun@redhat.com>
2024-11-05 11:23:15 +1300

    Add release transition script
    Signed-off-by: Robert Young <robeyoun@redhat.com>

---
+++ .github/scripts/setVersions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# usage: setVersions.sh pomVersion imageVersion [gitTagVersion]
+# if gitTagVersion is provided then a tag will be created after the changes are committed
+
+if [[ $# != 2 ]]; then
+    echo "Illegal number of parameters" >&2
+    exit 1
+fi
+mvn -B versions:set -DgenerateBackupPoms=false -DnewVersion=${1}
+sed --in-place --regexp-extended "s|flink-examples-data-generator:([^[:space:]]*)|flink-examples-data-generator:${2}|g" recommendation-app/data-generator.yaml
+sed --in-place --regexp-extended "s|flink-examples-data-generator:([^[:space:]]*)|flink-examples-data-generator:${2}|g" interactive-etl/data-generator.yaml
